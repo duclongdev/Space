9e12575d17cdc20d411a476cc566c73ca78ee873
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2022-04-01 22:00:35 +0700

    Xong giao diện đăng nhập

--- app/src/main/java/com/example/space/Login.java
+++ app/src/main/java/com/example/space/Login.java
@@ -2,8 +2,11 @@ package com.example.space;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -14,5 +17,21 @@ public class Login extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+        TextView btn_SI=findViewById(R.id.btn_SI);
+        LinearLayout btn_SU=findViewById(R.id.btn_SU);
+        btn_SI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Login.this,LoginScreen.class);
+                startActivity(intent);
+            }
+        });
+        btn_SU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Login.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/space/LoginScreen.java
@@ -0,0 +1,17 @@
+package com.example.space;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+}
