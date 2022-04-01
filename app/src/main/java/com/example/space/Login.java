cb4f470ac85e4c08675276fa396f8646ca961aff
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2022-04-01 17:21:07 +0700

    Mới xong giao diện đăng nhập

--- app/src/main/java/com/example/space/Login.java
+++ app/src/main/java/com/example/space/Login.java
@@ -3,6 +3,7 @@ package com.example.space;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 }
