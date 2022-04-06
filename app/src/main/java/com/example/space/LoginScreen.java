4bedc0cbf92859466bdab79fd5ed456c964f133c
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2022-04-06 13:59:17 +0700

    xong đăng nhập

--- app/src/main/java/com/example/space/LoginScreen.java
+++ app/src/main/java/com/example/space/LoginScreen.java
@@ -1,11 +1,22 @@
 package com.example.space;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginScreen extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +24,40 @@ public class LoginScreen extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+        mAuth = FirebaseAuth.getInstance();
+        EditText email=findViewById(R.id.enterEmail);
+        EditText password=findViewById(R.id.enterPassword);
+        Button btn_sign_in=findViewById(R.id.btn_sign_in);
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignIn(email.getText().toString().trim(),password.getText().toString().trim());
+            }
+        });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private void reload() {
+    }
+    private void SignIn(String email,String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginScreen.this, "thành công", Toast.LENGTH_SHORT).show();
+                        } else {
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/space/register/EnterName.java
+++ app/src/main/java/com/example/space/register/EnterName.java
@@ -11,17 +11,26 @@ import androidx.navigation.Navigation;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.space.R;
 import com.example.space.model.RegisterViewModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.concurrent.Executor;
 
 public class EnterName extends Fragment {
 
@@ -32,11 +41,13 @@ public class EnterName extends Fragment {
     NavController navController;
     boolean checkEmpty = true;
     boolean checkLength = true;
-
+    private FirebaseAuth mAuth;
+    private static final String TAG = "EmailPassword";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         model = new ViewModelProvider(requireActivity()).get(RegisterViewModel.class);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -50,10 +61,14 @@ public class EnterName extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         init(view);
+        String email=model.getEmail().getValue();
+        String password=model.getPassWord().getValue();
         done_backToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 checkEnterName();
+                model.getName().setValue(enterName.getText().toString());
+                createAccount(email,password);
             }
         });
 
@@ -90,6 +105,38 @@ public class EnterName extends Fragment {
             enterNameLayout.setError("Please enter your real name");
         }
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            reload();
+        }
+    }
 
+    private void reload() {
+    }
 
+    private void createAccount(String email, String password) {
+        String name=model.getName().getValue();
+        String age=model.getAge().getValue();
+        Toast.makeText(getActivity(),email,Toast.LENGTH_LONG).show();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(requireActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User users=new User(name,age,email);
+                            FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getUid()).setValue(users);
+                            Log.d(TAG, "createUserWithEmail:success");
+                        } else {
+
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+    }
 }
