066eaa2b71ef358306a01201138fc159f9a01980
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2022-04-26 17:28:41 +0700

    chinh bottom bar

--- app/src/main/java/com/example/space/Home.java
+++ app/src/main/java/com/example/space/Home.java
@@ -2,6 +2,8 @@ package com.example.space;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -9,6 +11,7 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -18,11 +21,20 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 public class Home extends AppCompatActivity {
-
+    final Fragment homeScreen = new HomeScreen();
+    final Fragment searchScreen = new SearchScreen();
+    final Fragment favoriteScreen = new FavoriteScreen();
+    final Fragment userScreen = new UserScreen();
+    final FragmentManager fm = getSupportFragmentManager();
+    private BottomNavigationView bottom_bar;
+    Fragment active = homeScreen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 //        BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -32,5 +44,36 @@ public class Home extends AppCompatActivity {
 //        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 //        NavigationUI.setupWithNavController(navView, navController);
+        fm.beginTransaction().add(R.id.nav_host_fragment_activity_main, userScreen, "4").hide(userScreen).commit();
+        fm.beginTransaction().add(R.id.nav_host_fragment_activity_main, favoriteScreen, "3").hide(favoriteScreen).commit();
+        fm.beginTransaction().add(R.id.nav_host_fragment_activity_main, searchScreen, "2").hide(searchScreen).commit();
+        fm.beginTransaction().add(R.id.nav_host_fragment_activity_main,homeScreen, "1").commit();
+        bottom_bar=findViewById(R.id.bottom_nav);
+        bottom_bar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        fm.beginTransaction().hide(active).show(homeScreen).commit();
+                        active = homeScreen;
+                        return true;
+
+                    case R.id.search:
+                        fm.beginTransaction().hide(active).show(searchScreen).commit();
+                        active = searchScreen;
+                        return true;
+
+                    case R.id.favorite:
+                        fm.beginTransaction().hide(active).show(favoriteScreen).commit();
+                        active = favoriteScreen;
+                        return true;
+                    case R.id.user:
+                        fm.beginTransaction().hide(active).show(userScreen).commit();
+                        active = userScreen;
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
