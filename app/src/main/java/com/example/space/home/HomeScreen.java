d0d637a81a6e83d792048081d555a57449f0d5e8
longdepchai173 <longdepchai173@gmail.com>
2022-05-02 14:41:22 +0700

    xong phần lấy dữ liệu cho category

--- app/src/main/java/com/example/space/home/HomeScreen.java
+++ app/src/main/java/com/example/space/home/HomeScreen.java
@@ -31,6 +31,8 @@ import com.example.space.home.playLists.category.CategoryPlaylist;
 import com.example.space.home.playLists.category.CategoryPlaylistAdapter;
 import com.example.space.home.playLists.playlist.PlayList;
 import com.example.space.model.Artist;
+import com.example.space.model.Genre;
+import com.example.space.model.Theme;
 import com.example.space.myInterface.IClickAdvSlideShow;
 import com.example.space.myInterface.IClickPlayList;
 
@@ -46,24 +48,19 @@ public class HomeScreen extends Fragment {
 
     private FragmentHomeScreenBinding binding;
     private List<Advertisement> advList;
+    private List<Artist> artistList;
+    private List<Theme> themeList;
     private CategoryPlaylistAdapter categoryPlaylistAdapter;
+    private List<CategoryPlaylist> categoryPlaylists;
     private Dataservice dataservice;
-    private Handler handler = new Handler();
-    private Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            if (binding.advertiseSlide.getCurrentItem() == advList.size() - 1) {
-                binding.advertiseSlide.setCurrentItem(0);
-            } else {
-                binding.advertiseSlide.setCurrentItem(binding.advertiseSlide.getCurrentItem() + 1);
-            }
-        }
-    };
+    private AdvViewPageAdapter advViewPageAdapter;
+    private Handler handler;
+    private Runnable runnable;
+    private Bundle passIdPlayList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         binding = FragmentHomeScreenBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
@@ -71,58 +68,89 @@ public class HomeScreen extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        setSlideShow();
         requireActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        initAllValue();
         initToolBar();
-        getCategoryPlaylist();
+        initSlideShow();
+        InitCategoryPlaylist();
+        LoadAllDataForCategory();
     }
 
-    private void getCategoryPlaylist() {
+    private void initAllValue() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                if (binding.advertiseSlide.getCurrentItem() == advList.size() - 1) {
+                    binding.advertiseSlide.setCurrentItem(0);
+                } else {
+                    binding.advertiseSlide.setCurrentItem(binding.advertiseSlide.getCurrentItem() + 1);
+                }
+            }
+        };
+        dataservice = APIService.getService();
+        categoryPlaylists = new ArrayList<>();
+        advList = new ArrayList<>();
+    }
+
+
+    private void initToolBar() {
+        ((AppCompatActivity) getActivity()).setSupportActionBar(binding.toolbar2);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+    }
+
+
+    private void initSlideShow() {
+        advViewPageAdapter = new AdvViewPageAdapter(advList, HomeScreen.this, new IClickAdvSlideShow() {
+            @Override
+            public void onClickAdvItem(int id) {
+                Toast.makeText(getContext(), "id: " + id, Toast.LENGTH_SHORT).show();
+            }
+        });
+        binding.advertiseSlide.setAdapter(advViewPageAdapter);
+        binding.transitionIndicator.setViewPager(binding.advertiseSlide);
+        binding.advertiseSlide.setPageTransformer(new ZoomOutPageTransformer());
+        binding.advertiseSlide.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                handler.removeCallbacks(runnable);
+                handler.postDelayed(runnable, 3000);
+            }
+        });
+    }
+
+
+    private void InitCategoryPlaylist() {
         categoryPlaylistAdapter = new CategoryPlaylistAdapter(HomeScreen.this, new IClickPlayList() {
             @Override
             public void onClickPlayList(PlayList playList) {
                 Toast.makeText(requireContext(), playList.getName(), Toast.LENGTH_SHORT).show();
-                Bundle bundle = new Bundle();
-                bundle.putInt("id_playlist", playList.getId());
-                bundle.putString("name_playlist", playList.getName());
-                bundle.putString("url_playlist", playList.getUrl());
-                bundle.putInt("type_playlist", playList.getType());
-                NavHostFragment.findNavController(HomeScreen.this).navigate(R.id.action_homeScreen_to_playlistScreen, bundle);
+                InitBundle(playList);
+                NavHostFragment.findNavController(HomeScreen.this).navigate(R.id.action_homeScreen_to_playlistScreen, passIdPlayList);
             }
         });
-        categoryPlaylistAdapter.setData(getCategoryList());
+        categoryPlaylistAdapter.setData(categoryPlaylists);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireActivity(), RecyclerView.VERTICAL, false);
         binding.AllPlayList.setLayoutManager(linearLayoutManager);
         binding.AllPlayList.setAdapter(categoryPlaylistAdapter);
     }
 
+
+    private void LoadAllDataForCategory() {
+        loadArtistCategory();
+        loadThemeCategory();
+        setSlideShow();
+    }
+
     private void setSlideShow() {
-        dataservice= APIService.getService();
-        Call<List<Advertisement>> call=dataservice.getBanner();
+        Call<List<Advertisement>> call = dataservice.getBanner();
         call.enqueue(new Callback<List<Advertisement>>() {
             @Override
             public void onResponse(Call<List<Advertisement>> call, Response<List<Advertisement>> response) {
-                advList=response.body();
-                AdvViewPageAdapter advViewPageAdapter = new AdvViewPageAdapter(advList, HomeScreen.this, new IClickAdvSlideShow() {
-                    @Override
-                    public void onClickAdvItem(int id) {
-                        Toast.makeText(getContext(), "id: " + id, Toast.LENGTH_SHORT).show();
-                    }
-                });
-                binding.advertiseSlide.setAdapter(advViewPageAdapter);
-                binding.transitionIndicator.setViewPager(binding.advertiseSlide);
-                binding.advertiseSlide.setPageTransformer(new ZoomOutPageTransformer());
-
-                binding.advertiseSlide.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-                    @Override
-                    public void onPageSelected(int position) {
-                        super.onPageSelected(position);
-                        handler.removeCallbacks(runnable);
-                        handler.postDelayed(runnable, 3000);
-                    }
-                });
+                advList.addAll(response.body());
+                advViewPageAdapter.notifyDataSetChanged();
             }
-
             @Override
             public void onFailure(Call<List<Advertisement>> call, Throwable t) {
 
@@ -131,9 +159,57 @@ public class HomeScreen extends Fragment {
 
     }
 
-    private void initToolBar() {
-        ((AppCompatActivity) getActivity()).setSupportActionBar(binding.toolbar2);
-        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+
+    private void loadArtistCategory() {
+        artistList = new ArrayList<>();
+        List<PlayList> playLists = new ArrayList<>();
+        Call<List<Artist>> call = dataservice.getArtist();
+        call.enqueue(new Callback<List<Artist>>() {
+            @Override
+            public void onResponse(Call<List<Artist>> call, Response<List<Artist>> response) {
+                artistList.addAll(response.body());
+                for (Artist artist : artistList) {
+                    playLists.add(new PlayList(Integer.parseInt(artist.getIdArtist()), artist.getLinkImage(), artist.getName(), 2));
+                }
+                categoryPlaylists.add(new CategoryPlaylist(1, "Artist", playLists));
+                categoryPlaylistAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Artist>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void loadThemeCategory() {
+        themeList = new ArrayList<>();
+        List<PlayList> playLists = new ArrayList<>();
+        Call<List<Theme>> call = dataservice.getTheme();
+        call.enqueue(new Callback<List<Theme>>() {
+            @Override
+            public void onResponse(Call<List<Theme>> call, Response<List<Theme>> response) {
+                themeList.addAll(response.body());
+                for (Theme theme : themeList) {
+                    playLists.add(new PlayList(Integer.parseInt(theme.getIdTheme()), theme.getLinkTheme(), theme.getName(), 1));
+                }
+                categoryPlaylists.add(new CategoryPlaylist(1, "Playlist for new day", playLists));
+                categoryPlaylistAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Theme>> call, Throwable t) {
+
+            }
+        });
+    }
+    private void InitBundle(PlayList playList) {
+        passIdPlayList = new Bundle();
+        passIdPlayList.putInt("id_playlist", playList.getId());
+        passIdPlayList.putString("name_playlist", playList.getName());
+        passIdPlayList.putString("url_playlist", playList.getUrl());
+        passIdPlayList.putInt("type_playlist", playList.getType());
     }
 
     @Override
@@ -152,54 +228,5 @@ public class HomeScreen extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         binding = null;
-    }
-
-    private List<CategoryPlaylist> getCategoryList() {
-        List<CategoryPlaylist> categoryList = new ArrayList<>();
-        List<PlayList> list1 = new ArrayList<>();
-        list1.add(new PlayList(1, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihi", 1));
-        list1.add(new PlayList(2, "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihi", 1));
-        list1.add(new PlayList(3, "https://images.pexels.com/photos/1408221/pexels-photo-1408221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihih", 1));
-        list1.add(new PlayList(4, "https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "cc", 1));
-        list1.add(new PlayList(5, "https://images.pexels.com/photos/1114690/pexels-photo-1114690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHI", 1));
-        list1.add(new PlayList(7, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHIH", 1));
-        list1.add(new PlayList(8, "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "DASDA", 1));
-        list1.add(new PlayList(1, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihi", 1));
-        list1.add(new PlayList(4, "https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "cc", 1));
-        list1.add(new PlayList(5, "https://images.pexels.com/photos/1114690/pexels-photo-1114690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHI", 1));
-        list1.add(new PlayList(7, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHIH", 1));
-        list1.add(new PlayList(8, "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "DASDA", 1));
-        List<PlayList> list2 = new ArrayList<>();
-        list2.add(new PlayList(1, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihi", 2));
-        list2.add(new PlayList(2, "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihi", 2));
-        list2.add(new PlayList(3, "https://images.pexels.com/photos/1408221/pexels-photo-1408221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihih", 2));
-        list2.add(new PlayList(4, "https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "cc", 2));
-        list2.add(new PlayList(5, "https://images.pexels.com/photos/1114690/pexels-photo-1114690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHI", 2));
-        list2.add(new PlayList(7, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHIH", 2));
-        list2.add(new PlayList(8, "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "DASDA", 2));
-        list2.add(new PlayList(1, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihi", 2));
-        list2.add(new PlayList(4, "https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "cc", 2));
-        list2.add(new PlayList(5, "https://images.pexels.com/photos/1114690/pexels-photo-1114690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHI", 2));
-        list2.add(new PlayList(7, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHIH", 2));
-        list2.add(new PlayList(8, "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "DASDA", 2));
-        List<PlayList> list0 = new ArrayList<>();
-        list0.add(new PlayList(1, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihi", 0));
-        list0.add(new PlayList(2, "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihi", 0));
-        list0.add(new PlayList(3, "https://images.pexels.com/photos/1408221/pexels-photo-1408221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihih", 0));
-        list0.add(new PlayList(4, "https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "cc", 0));
-        list0.add(new PlayList(5, "https://images.pexels.com/photos/1114690/pexels-photo-1114690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHI", 0));
-        list0.add(new PlayList(7, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHIH", 0));
-        list0.add(new PlayList(8, "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "DASDA", 0));
-        list0.add(new PlayList(1, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "hihi", 0));
-        list0.add(new PlayList(4, "https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "cc", 0));
-        list0.add(new PlayList(5, "https://images.pexels.com/photos/1114690/pexels-photo-1114690.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHI", 0));
-        list0.add(new PlayList(7, "https://images.pexels.com/photos/2246476/pexels-photo-2246476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "HIHIH", 0));
-        list0.add(new PlayList(8, "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", "DASDA", 0));
-        categoryList.add(new CategoryPlaylist(1, "Recent playlist", list0));
-        categoryList.add(new CategoryPlaylist(2, "Greeting the new day", list1));
-        categoryList.add(new CategoryPlaylist(3, "Today's selection", list1));
-        categoryList.add(new CategoryPlaylist(4, "Featured singer", list2));
-        categoryList.add(new CategoryPlaylist(5, "New music every day", list1));
-        return categoryList;
     }
 }
