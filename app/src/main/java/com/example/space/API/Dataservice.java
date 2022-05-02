923b7266c937564ef248ee69c0e8d8926f5452e5
iawtk2302 <86036919+iawtk2302@users.noreply.github.com>
2022-05-02 10:41:41 +0700

    ghép màn hình home với search

--- app/src/main/java/com/example/space/API/Dataservice.java
+++ app/src/main/java/com/example/space/API/Dataservice.java
@@ -1,5 +1,7 @@
 package com.example.space.API;
 
+import com.example.space.home.advSlide.Advertisement;
+import com.example.space.model.Artist;
 import com.example.space.model.Genre;
 import com.example.space.model.Song;
 import com.example.space.model.Theme;
@@ -17,6 +19,8 @@ public interface Dataservice {
     Call<List<Song>> getRecommendSong();
     @GET("theme.php")
     Call<List<Theme>> getTheme();
-    @GET("genre.php")
-    Call<List<Genre>> getGenre();
+    @GET("banner.php")
+    Call<List<Advertisement>> getBanner();
+    @GET("artist.php")
+    Call<List<Artist>> getArtist();
 }
--- app/src/main/java/com/example/space/home/advSlide/Advertisement.java
+++ app/src/main/java/com/example/space/home/advSlide/Advertisement.java
@@ -1,19 +1,44 @@
 package com.example.space.home.advSlide;
 
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
 public class Advertisement {
-    public Advertisement(int id, String url) {
-        this.id = id;
-        this.url = url;
+
+    @SerializedName("idBanner")
+    @Expose
+    private String idBanner;
+    @SerializedName("idSong")
+    @Expose
+    private String idSong;
+    @SerializedName("linkImage")
+    @Expose
+    private String linkImage;
+
+    public String getIdBanner() {
+        return idBanner;
     }
 
-    public int getId() {
-        return id;
+    public void setIdBanner(String idBanner) {
+        this.idBanner = idBanner;
     }
 
-    public String getUrl() {
-        return url;
+    public String getIdSong() {
+        return idSong;
     }
 
-    private int id;
-    private String url;
+    public void setIdSong(String idSong) {
+        this.idSong = idSong;
+    }
+
+    public String getLinkImage() {
+        return linkImage;
+    }
+
+    public void setLinkImage(String linkImage) {
+        this.linkImage = linkImage;
+    }
+
 }
