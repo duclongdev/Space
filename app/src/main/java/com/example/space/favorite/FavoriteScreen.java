daa4309f304d9e7ea1dd9d2cb99e614270f5a8f4
longdepchai173 <longdepchai173@gmail.com>
2022-05-20 20:50:17 +0700

    hihi

--- app/src/main/java/com/example/space/favorite/FavoriteScreen.java
+++ app/src/main/java/com/example/space/favorite/FavoriteScreen.java
@@ -15,16 +15,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
+import com.example.space.API.APIService;
+import com.example.space.API.Dataservice;
 import com.example.space.R;
 import com.example.space.databinding.FragmentFavoriteScreenBinding;
 import com.example.space.favorite.songs.SongsFavoriteAdapter;
 import com.example.space.model.Song;
 import com.example.space.myInterface.IClickSong;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class FavoriteScreen extends Fragment {
 
-
+    private String UId;
     private FragmentFavoriteScreenBinding binding;
     private SongsFavoriteAdapter songsFavoriteAdapter;
 
@@ -38,6 +47,8 @@ public class FavoriteScreen extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        UId = FirebaseAuth.getInstance().getCurrentUser().getUid().toString();
+
         songsFavoriteAdapter = new SongsFavoriteAdapter(FavoriteScreen.this, new IClickSong() {
             @Override
             public void onCLickSong(Song song) {
@@ -47,9 +58,24 @@ public class FavoriteScreen extends Fragment {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false);
         binding.songsFvr.setLayoutManager(linearLayoutManager);
         binding.songsFvr.setAdapter(songsFavoriteAdapter);
-        songsFavoriteAdapter.setData(setData());
+        setData();
     }
 
     void setData(){
+        Dataservice dataservice = APIService.getService();
+        Call<List<Song>> call = dataservice.getSongFavorite(UId);
+        call.enqueue(new Callback<List<Song>>() {
+            @Override
+            public void onResponse(Call<List<Song>> call, Response<List<Song>> response) {
+                List<Song> songs = response.body();
+                songsFavoriteAdapter.setData(songs);
+                songsFavoriteAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Song>> call, Throwable t) {
+
+            }
+        });
     }
 }
